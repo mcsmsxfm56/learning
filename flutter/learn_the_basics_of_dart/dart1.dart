13fbe054dc5904bd2566169144eecda212f928f2
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-06-03 17:22:30 -0300

    initial commit

---
+++ flutter/learn_the_basics_of_dart/dart1.dart
@@ -0,0 +1,50 @@
+//se importa el modulo de matematicas como Random, es como el import as de javascript solo que este es import show
+import 'dart:math' show Random;
+
+//funcion asincrona
+void main() async {
+  /*print imprime en pantalla al igual que python, el metodo de montecarlo es un metodo para aproximar expresiones matematicas
+  complejas y costosas de evaluar*/
+  print('Compute π using the Monte Carlo method.');
+  await for (final estimate in computePi().take(100)) {
+    print('π ≅ $estimate');
+  }
+}
+
+/// Generates a stream of increasingly accurate estimates of π.
+//Stream es una de las clases que da soporte para programacion asincronica
+Stream<double> computePi({int batch = 100000}) async* {
+  var total = 0; // Inferred to be of type int
+  var count = 0;
+  while (true) {
+    final points = generateRandom().take(batch);
+    final inside = points.where((p) => p.isInsideUnitCircle);
+
+    total += batch;
+    count += inside.length;
+    final ratio = count / total;
+
+    // Area of a circle is A = π⋅r², therefore π = A/r².
+    // So, when given random points with x ∈ <0,1>,
+    // y ∈ <0,1>, the ratio of those inside a unit circle
+    // should approach π / 4. Therefore, the value of π
+    // should be:
+    yield ratio * 4;
+  }
+}
+
+Iterable<Point> generateRandom([int? seed]) sync* {
+  final random = Random(seed);
+  while (true) {
+    yield Point(random.nextDouble(), random.nextDouble());
+  }
+}
+
+class Point {
+  final double x;
+  final double y;
+
+  const Point(this.x, this.y);
+
+  bool get isInsideUnitCircle => x * x + y * y <= 1;
+}
---
+++ flutter/learn_the_basics_of_dart/dart2.dart
@@ -0,0 +1,13 @@
+void main() { //se declara funcion  main
+  for (int i = 0; i < 5; i++) { //bucle for, se declara la variable i con tipado integer
+  //i sera igual a 0, 1, 2, 3, 4
+    print('hello ${i + 1}');  //se imprime el resultado + 1, se puede meter codigo ejecutable en un string
+    //al igual que javascript con ${}
+    //hello 1
+    //hello 2
+    //hello 3
+    //hello 4
+    //hello 5
+    //esto se corre en consola con dart dart2.dart
+  }  
+} 
